736b621bda2639fa8c1de6e5640d0db629866ba7
Nicolas Martins <nicolas.edm7@gmail.com>
2024-11-06 10:11:48 -0300

    Completando desafio 4 java praticando o uso de interface

--- src/main/java/dev/nicolasmartins/Main.java
+++ src/main/java/dev/nicolasmartins/Main.java
@@ -4,6 +4,16 @@ package dev.nicolasmartins;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("==========NINJA BÁSICO==========\n");
 
+        NinjaBasico ninjaBasico = new NinjaBasico("Konohamaru Sarutobi", 15, "ser um inútil", NinjaBasico.TipoHabilidade.NINJUTSU);
+        ninjaBasico.mostrarInformacoes();
+        ninjaBasico.executarHabilidade();
+
+        System.out.println("\n\n========NINJA AVANÇADO=========\n");
+
+        NinjaAvancado ninjaAvancado = new NinjaAvancado("Galvão Bueno", 65, "RRRRONALDINHO", NinjaBasico.TipoHabilidade.GENJUTSU, "TAFAREEEEEEEEEEEEEL");
+        ninjaAvancado.mostrarInformacoes();
+        ninjaAvancado.executarHabilidade();
     }
 }
